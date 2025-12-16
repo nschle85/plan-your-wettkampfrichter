758b6c1e748f00d309e38bbeefbda69ab53db15e
Norman Schleicher <normanschleicher@gmx.de>
2025-12-16 19:37:34 +0100

    remove redundant local modifications for user list

--- client/src/components/user-list/user-list.component.ts
+++ client/src/components/user-list/user-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../services/api.service';
 import { User } from '../../models/types';
+import { SocketService } from '../../services/socket.service';
 
 @Component({
   standalone: true,
@@ -10,13 +11,32 @@ import { User } from '../../models/types';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent {
+export class UserListComponent implements OnDestroy {
   users: User[] = [];
   loading = true;
   error: string | null = null;
 
-  constructor(private api: ApiService) {
+  private onUsersUpdate = (msg: any) => {
+    if (msg?.type === 'user:add' && msg.user) {
+      const u: User = msg.user as User;
+      if (!this.users.some(x => x.id === u.id)) {
+        this.users = [...this.users, u];
+      }
+    }
+    if (msg?.type === 'user:remove' && msg.userId != null) {
+      const id = Number(msg.userId);
+      this.users = this.users.filter(x => x.id !== id);
+    }
+  };
+
+  constructor(private api: ApiService, private socket: SocketService) {
     this.load();
+    // Subscribe to global users updates
+    this.socket.on('users:update', this.onUsersUpdate);
+  }
+
+  ngOnDestroy(): void {
+    this.socket.off('users:update', this.onUsersUpdate);
   }
 
   load() {
@@ -35,8 +55,7 @@ export class UserListComponent {
     this.error = null;
     this.api.deleteUser(u.id).subscribe({
       next: () => {
-        // Lokales Entfernen, um kein zusätzliches Laden zu benötigen
-        this.users = this.users.filter(x => x.id !== u.id);
+        // Socket-Event 'users:update' mit type 'user:remove' wird die Liste aktualisieren
         this.loading = false;
       },
       error: () => {
